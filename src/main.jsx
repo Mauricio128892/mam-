9d88d3361bc7de5cfeee28790a6c29c9f5045bad
unknown <mnvalladares05@gmail.com>
2025-08-03 17:35:03 -0600

    Feat: Configuración de Tailwind CSS y corrección de estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Asegúrate de que esta ruta sea correcta
-import './index.css'; // O la ruta a tu archivo CSS principal
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* Aquí NO debe haber un <BrowserRouter> o <Router> envolviendo a <App /> */}
+    <App />
   </React.StrictMode>,
 );
